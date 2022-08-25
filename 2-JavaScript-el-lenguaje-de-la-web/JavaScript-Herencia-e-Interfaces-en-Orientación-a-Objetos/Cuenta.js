31b4d98e41b931dbd2e7ef92a095e2b2a788687f
Horacio Carlos Maciel de Lima <95258782+carlos2010maciel@users.noreply.github.com>
2022-08-24 21:41:33 -0300

    Identificando clases abstractas

--- 2-JavaScript-el-lenguaje-de-la-web/JavaScript-Herencia-e-Interfaces-en-Orientación-a-Objetos/Cuenta.js
+++ 2-JavaScript-el-lenguaje-de-la-web/JavaScript-Herencia-e-Interfaces-en-Orientación-a-Objetos/Cuenta.js
@@ -3,10 +3,23 @@ export class Cuenta {
     #saldo;
 
     constructor(cliente, numero, agencia, saldo) {
+
+        if (this.constructor == Cuenta) {
+            throw new Error('No se debe instanciar objetos de la clase Cuenta');
+        }
         this.numero = numero;
         this.agencia = agencia;
         this.#cliente = cliente;
         this.#saldo = saldo;
+    }
+
+    set cliente(valor) {
+        if (valor instanceof Cliente)
+            this.#cliente = valor;
+    }
+
+    get cliente() {
+        return this.#cliente;
     }
 
     depositoEnCuenta(valor) {
@@ -16,6 +29,12 @@ export class Cuenta {
     }
 
     retirarDeCuenta(valor) {
+        //Método abstracto
+        throw new Error('Debe implementar el método retirarDeCuenta en su clase');
+    }
+
+    _retirarDeCuenta(valor, comision) {
+        valor = valor * (1+comision/100);
         if (valor <= this.#saldo)
             this.#saldo -= valor;
         return this.#saldo;
@@ -30,5 +49,9 @@ export class Cuenta {
         cuentaDestino.depositoEnCuenta(valor);
         valor = 200;
         valor = valor*1000;
+    }
+
+    prueba() {
+        console.log('Método padre');
     }
 }
